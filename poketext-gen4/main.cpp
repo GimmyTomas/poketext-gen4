5446cd4e03ff73f6529c4620d5320917ca270653
Giovanni Maria Tomaselli <gimmytomas@gmail.com>
2024-01-19 23:37:17 +0100

    Initial commit

---
+++ poketext-gen4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  poketext-gen4
+//
+//  Created by Giovanni Maria Tomaselli on 19/01/24.
+//
+
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0;
+}
